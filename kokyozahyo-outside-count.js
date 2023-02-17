1570a901d4100771eed21fb1f39e837da9e7c77e
naoki <n.globe.us@gmail.com>
2023-02-18 02:36:51 +0900

    some fix

---
+++ kokyozahyo-outside-count.js
@@ -0,0 +1,76 @@
+#!/usr/bin/env node
+const glob = require("glob")
+const fs = require("fs")
+const turf = require("@turf/turf");
+const { createArrayCsvWriter } = require('csv-writer')
+const { updateLatestCityCode } = require("./util/update-latest-city-code")
+const progressBar = require("progress-bar-cli");
+let startTime = new Date();
+const outsideFiles = []
+
+const args = process.argv.slice(2)
+const prefCode = args[0] // 都道府県コードを第一引数で指定する
+
+// const files = glob.sync(`../all_zips/${prefCode}*.ndgeojson`);
+const files = glob.sync(`./test/${prefCode}*.ndgeojson`);
+
+// 地番住所の ndgeojson ファイルを読み込む
+for (const file of files) {
+  
+  // progressBar.progressBar(files.indexOf(file), files.length, startTime);
+  const raw = fs.readFileSync(file, "utf8");
+  const features = raw.split("\n")
+  let is筆InsideCity = false;
+  let unionPolygon = turf.polygon([[[0, 0], [0, 0], [0, 0], [0, 0]]])
+
+  // 筆ごとに処理する
+  for (const raw of features) {
+
+    if (!raw) {
+      continue;
+    }
+
+    const 筆feature = JSON.parse(raw)
+
+    if (!筆feature.properties.地番.match(/^[0-9]/)) {
+      continue;
+    }
+    unionPolygon = turf.union(unionPolygon, 筆feature)
+  }
+
+  const basename = file.split("/").pop().split(".")[0]
+  const code = updateLatestCityCode(basename.split("-")[0])
+
+  let cityData;
+  // ファイルが存在するかチェックする
+  try {
+    cityData = fs.readFileSync(`./data/admins/${prefCode}/${code}.json`, "utf8");
+  } catch (e) {
+    console.log(`./data/admins/${prefCode}/${code}.json が存在しません`)
+    continue;
+  }
+
+  const city = JSON.parse(cityData)
+
+  // 市区町村ポリゴンをループする
+  for (const cityFeature of city.features) {
+
+    is筆InsideCity = turf.booleanWithin(unionPolygon, cityFeature)
+
+    if (!is筆InsideCity) {
+      break
+    }
+  }
+  
+  if (!is筆InsideCity) {
+    outsideFiles.push([`${basename}.zip`])
+    break;
+  }
+}
+
+const csvWriterOutside = createArrayCsvWriter({
+  path: `./output/${prefCode}_all_kyokyozahyo_outside_files.csv`,
+  header: ['zip_file']
+})
+csvWriterOutside.writeRecords(outsideFiles)
+
