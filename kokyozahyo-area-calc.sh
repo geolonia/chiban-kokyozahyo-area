33ddeb5df8e0b6c572c47b343a1613d2024a480e
naoki <n.globe.us@gmail.com>
2023-02-17 12:02:43 +0900

    some fix

--- kokyozahyo-area-calc.sh
+++ kokyozahyo-area-calc.sh
@@ -1,40 +1,36 @@
 #!/usr/bin/env bash
 set -e
 
+if [ ! -d output ]; then
+  mkdir output
+else
+  rm -f output/*_city_kokyozahyo_area.csv
+  rm -f output/*_pref_kokyozahyo_area.csv 
+fi
+
 for i in {1..47}
 do
   num=$(printf "%02d" $i)
   echo node kokyozahyo-area-calc.js $num
 done | parallel
 
-# TARGET_DIR=output
+TARGET_DIR=output
+CSV_HEADER="code, kokyozahyo_area"
 
-# # *_all_kyokyozahyo_outside_files.csv を結合する
-# ALL_OUTSIDE_FILE=$TARGET_DIR/all_kokyozahyo_outside.csv
-# CSV_HEADER_OUTSIDE="code,zip_file,市区町村名,地番"
+# *_pref_kyokyozahyo_outside_files.csv を結合する
+PREF_COUNT_FILE=$TARGET_DIR/pref_kokyozahyo_area.csv
 
-# cat $TARGET_DIR/*_all_kyokyozahyo_outside_files.csv > $ALL_OUTSIDE_FILE
-# sed -i "" "s/$CSV_HEADER_OUTSIDE//" $ALL_OUTSIDE_FILE
-# sed -i "" "/^$/d" $ALL_OUTSIDE_FILE
-# find . -name "*_all_kyokyozahyo_outside_files.csv" | xargs rm
-# sed -i "" "1s/^/$CSV_HEADER_OUTSIDE\n/" $ALL_OUTSIDE_FILE
+cat $TARGET_DIR/*_pref_kokyozahyo_area.csv > $PREF_COUNT_FILE
+sed -i "" "s/$CSV_HEADER//" $PREF_COUNT_FILE
+sed -i "" "/^$/d" $PREF_COUNT_FILE
+find . -name "*_pref_kokyozahyo_area.csv" | xargs rm
+sed -i "" "1s/^/$CSV_HEADER\n/" $PREF_COUNT_FILE
 
-# # *_pref_kyokyozahyo_outside_files.csv を結合する
-# PREF_COUNT_FILE=$TARGET_DIR/pref_kokyozahyo_outside.csv
-# CSV_HEADER_COUNT="code,kokyozahyo_total,kokyozahyo_outside"
+# *_city_kyokyozahyo_outside_files.csv を結合する
+CITY_COUNT_FILE=$TARGET_DIR/city_kokyozahyo_area.csv
 
-# cat $TARGET_DIR/*_pref_kokyozahyo_outside.csv > $PREF_COUNT_FILE
-# sed -i "" "s/$CSV_HEADER_COUNT//" $PREF_COUNT_FILE
-# sed -i "" "/^$/d" $PREF_COUNT_FILE
-# find . -name "*_pref_kokyozahyo_outside.csv" | xargs rm
-# sed -i "" "1s/^/$CSV_HEADER_COUNT\n/" $PREF_COUNT_FILE
-
-# # *_city_kyokyozahyo_outside_files.csv を結合する
-# CITY_COUNT_FILE=$TARGET_DIR/city_kokyozahyo_outside.csv
-# CSV_HEADER_COUNT="code,kokyozahyo_total,kokyozahyo_outside"
-
-# cat $TARGET_DIR/*_city_kokyozahyo_outside.csv > $CITY_COUNT_FILE
-# sed -i "" "s/$CSV_HEADER_COUNT//" $CITY_COUNT_FILE
-# sed -i "" "/^$/d" $CITY_COUNT_FILE
-# find . -name "*_city_kokyozahyo_outside.csv" | xargs rm
-# sed -i "" "1s/^/$CSV_HEADER_COUNT\n/" $CITY_COUNT_FILE
+cat $TARGET_DIR/*_city_kokyozahyo_area.csv > $CITY_COUNT_FILE
+sed -i "" "s/$CSV_HEADER//" $CITY_COUNT_FILE
+sed -i "" "/^$/d" $CITY_COUNT_FILE
+find . -name "*_city_kokyozahyo_area.csv" | xargs rm
+sed -i "" "1s/^/$CSV_HEADER\n/" $CITY_COUNT_FILE
