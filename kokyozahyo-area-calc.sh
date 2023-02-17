1cd7e7d772896357db20f05496352f32ddeb209c
naoki <n.globe.us@gmail.com>
2023-02-17 12:39:00 +0900

    some fix

--- kokyozahyo-area-calc.sh
+++ kokyozahyo-area-calc.sh
@@ -17,23 +17,30 @@ done | parallel
 TARGET_DIR=output
 CSV_HEADER="code,kokyozahyo_area"
 
+# mac だと -i "" にする、linux の場合は、-i だけでいい
+SED_OPTION="-i"
+
+if [ "$(uname)" == 'Darwin' ]; then
+  SED_OPTION="-i \"\""
+fi
+
 # *_pref_kyokyozahyo_outside_files.csv を結合する
 PREF_COUNT_FILE=$TARGET_DIR/pref_kokyozahyo_area.csv
 
 cat $TARGET_DIR/*_pref_kokyozahyo_area.csv > $PREF_COUNT_FILE
-sed -i "" "s/$CSV_HEADER//" $PREF_COUNT_FILE
-sed -i "" "/^$/d" $PREF_COUNT_FILE
+sed $SED_OPTION "s/$CSV_HEADER//" $PREF_COUNT_FILE
+sed $SED_OPTION "/^$/d" $PREF_COUNT_FILE
 find . -name "*_pref_kokyozahyo_area.csv" | xargs rm
-sed -i "" "1s/^/$CSV_HEADER\n/" $PREF_COUNT_FILE
+sed $SED_OPTION "1s/^/$CSV_HEADER\n/" $PREF_COUNT_FILE
 
 # *_city_kyokyozahyo_outside_files.csv を結合する
 CITY_COUNT_FILE=$TARGET_DIR/city_kokyozahyo_area.csv
 
 cat $TARGET_DIR/*_city_kokyozahyo_area.csv > $CITY_COUNT_FILE
-sed -i "" "s/$CSV_HEADER//" $CITY_COUNT_FILE
-sed -i "" "/^$/d" $CITY_COUNT_FILE
+sed $SED_OPTION "s/$CSV_HEADER//" $CITY_COUNT_FILE
+sed $SED_OPTION "/^$/d" $CITY_COUNT_FILE
 find . -name "*_city_kokyozahyo_area.csv" | xargs rm
-sed -i "" "1s/^/$CSV_HEADER\n/" $CITY_COUNT_FILE
+sed $SED_OPTION "1s/^/$CSV_HEADER\n/" $CITY_COUNT_FILE
 
 rm -f output/*_city_kokyozahyo_area.csv
 rm -f output/*_pref_kokyozahyo_area.csv
