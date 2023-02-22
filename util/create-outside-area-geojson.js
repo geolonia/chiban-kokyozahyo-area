fe1e047473ed19d487e1467f2211264a7ec992f7
naoki <n.globe.us@gmail.com>
2023-02-22 16:08:45 +0900

    rewrite by stream

--- util/create-outside-area-geojson.js
+++ util/create-outside-area-geojson.js
@@ -6,25 +6,45 @@ const { parse } = require('csv-parse/sync')
 const targetPath = path.join(__dirname, '../output/all_kokyozahyo_outside.csv')
 // const targetPath = path.join(__dirname, '../test/all_kokyozahyo_outside.csv')
 
+const outputPath = path.join(__dirname, '../output/outside-area.ndgeojson');
+// const outputPath = path.join(__dirname, '../test/outside-area.ndgeojson');
+
 let outsideAreaGeoJson = ''
 
-if (!fs.existsSync(targetPath)) {
-  console.error('File not found: ' + targetPath)
-  process.exit(1)
+async function processLineByLine() {
+
+  if (!fs.existsSync(targetPath)) {
+    console.error('File not found: ' + targetPath)
+    process.exit(1)
+  }
+
+  const file = fs.readFileSync(targetPath, 'utf-8');
+  const csv = parse(file, { columns: true, skip_empty_lines: true });
+  const streamWrite = fs.createWriteStream(outputPath);
+
+  for (const row of csv) {
+    const { zip_file } = row
+
+    const basename = path.basename(zip_file, '.zip')
+    const filePath = path.join(__dirname, '../../all_zips', basename + '.ndgeojson')
+    // const filePath = path.join(__dirname, '../test', basename + '.ndgeojson')
+
+    const fileStream = fs.createReadStream(filePath);
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity,
+    });
+
+    for await (const line of rl) {
+      console.log(`Line from file: ${line}`);
+      streamWrite.write(line);
+
+    }
+  }
+
+  streamWrite.end();
+  console.log('done');
+
 }
 
-const file = fs.readFileSync(targetPath, 'utf-8');
-const csv = parse(file, { columns: true, skip_empty_lines: true });
-
-for (const row of csv) {
-  const { zip_file } = row
-
-  const basename = path.basename(zip_file, '.zip')
-  const filePath = path.join(__dirname, '../../all_zips', basename + '.ndgeojson')
-  // const filePath = path.join(__dirname, '../test', basename + '.ndgeojson')
-  const raw = fs.readFileSync(filePath, 'utf-8')
-  outsideAreaGeoJson += raw
-}
-
-fs.writeFileSync(path.join(__dirname, '../output/outside-area.ndgeojson'), outsideAreaGeoJson)
-// fs.writeFileSync(path.join(__dirname, '../test/outside-area.ndgeojson'), outsideAreaGeoJson)
+processLineByLine();
