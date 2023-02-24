18c39cab6e8507199106bd761fc54331dfdc47ef
naoki <n.globe.us@gmail.com>
2023-02-24 17:47:29 +0900

    some fix

--- util/create-outside-area-tile.sh
+++ util/create-outside-area-tile.sh
@@ -8,6 +8,6 @@ INPUT_FILE="${BASE_DIR}/output/outside-area.ndgeojson"
 OUTPUT_FILE="${BASE_DIR}/outside-area-files.mbtiles"
 # --temporary-directoryオプションでマウントしたディレクトリに一時ファイルを作成する。
 # でないとディスク容量不足で失敗する。
-tippecanoe_args="-zg --force -P --temporary-directory=${BASE_DIR} --drop-densest-as-needed -x 筆ID -x version -x 測地系判別 -x 地図名 -x 地図番号 -x 縮尺分母 -x 大字コード -x 丁目コード -x 小字コード -x 予備コード -x 大字名 -x 丁目名 -x 小字名 -x 予備名 -x 精度区分 -x 座標値種別 -x 図上測量 -x 筆界未定構成筆"
+tippecanoe_args="-zg --force -P --temporary-directory=${BASE_DIR} --drop-fraction-as-needed -x 筆ID -x version -x 測地系判別 -x 地図名 -x 地図番号 -x 縮尺分母 -x 大字コード -x 丁目コード -x 小字コード -x 予備コード -x 大字名 -x 丁目名 -x 小字名 -x 予備名 -x 精度区分 -x 座標値種別 -x 図上測量 -x 筆界未定構成筆"
 
 tippecanoe ${tippecanoe_args} -o ${OUTPUT_FILE} -l outside-area $INPUT_FILE
