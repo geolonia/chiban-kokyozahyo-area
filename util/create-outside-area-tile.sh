56512982fe4659ec4ec85e8eda097f608c4b90fd
naoki <n.globe.us@gmail.com>
2023-02-28 11:13:48 +0900

    update tile generate script

--- util/create-outside-area-tile.sh
+++ util/create-outside-area-tile.sh
@@ -8,24 +8,37 @@ BASE_DIR=$(cd ${SCRIPT_DIR}/..; pwd)
 INPUT_FILE="${BASE_DIR}/output/outside-area.ndgeojson"
 OUTPUT_FILE="${BASE_DIR}/outside-area-files.mbtiles"
 
-find $BASE_DIR -name 'outside-area.ndgeojson' -print0 | parallel -0 -j 16 --line-buffer jq -cr -f $SCRIPT_DIR/polygon_filter_script.jq '{}' > ./all_polygons.ndgeojson
-find $BASE_DIR -name 'outside-area.ndgeojson' -print0 | parallel -0 -j 16 --line-buffer jq -cr -f $SCRIPT_DIR/point_filter_script.jq '{}' > ./all_points.ndgeojson
+# unix time を出力
+echo "start $(date +%s)"
 
-# --temporary-directoryオプションでマウントしたディレクトリに一時ファイルを作成する。
-# でないとディスク容量不足で失敗する。
-# tippecanoe_args="-Z 7 -z 20 --force -P --temporary-directory=${BASE_DIR} --drop-densest-as-needed -x 筆ID -x version -x 測地系判別 -x 地図名 -x 地図番号 -x 縮尺分母 -x 大字コード -x 丁目コード -x 小字コード -x 予備コード -x 大字名 -x 丁目名 -x 小字名 -x 予備名 -x 精度区分 -x 座標値種別 -x 図上測量 -x 筆界未定構成筆"
+# csvファイルのヘッダーを削除
+sed '1d' $BASE_DIR/output/outside_area_files.csv
 
-# tippecanoe ${tippecanoe_args} -o ${OUTPUT_FILE} -l outside-area $INPUT_FILE
+cat $BASE_DIR/output/outside_area_files.csv -v | parallel -0 -j 16 --line-buffer jq -cr -f $SCRIPT_DIR/polygon_filter_script.jq '{}' > ./all_polygons.ndgeojson
+cat $BASE_DIR/output/outside_area_files.csv -v | parallel -0 -j 16 --line-buffer ./xml_polygon_generator.sh '{}' > ./xml_polygons.ndgeojson
+cat $BASE_DIR/output/outside_area_files.csv -v | parallel -0 -j 16 --line-buffer jq -cr -f $SCRIPT_DIR/point_filter_script.jq '{}' > ./all_points.ndgeojson
 
 mkdir -p $(pwd)/tmp
 # ポイントは z0 - z11 まで
 tippecanoe \
-  -z14 -Z0 \
+  -z11 -Z0 \
   -r1 --cluster-distance=10 \
   --read-parallel \
   -f -o all_points.mbtiles \
   -t $(pwd)/tmp \
   -l outside-area ./all_points.ndgeojson
+
+# zipポリゴンは z0-13 まで
+tippecanoe \
+  -z13 -Z0 \
+  -pk \
+  --drop-smallest-as-needed \
+  -pS \
+  --detect-shared-borders \
+  --read-parallel \
+  -f -o xml_polygons.mbtiles \
+  -t $(pwd)/tmp \
+  -l outside-area ./xml_polygons.ndgeojson
 
 # ポリゴンは z14 - z15 まで
 tippecanoe \
@@ -40,4 +53,6 @@ tippecanoe \
   -t $(pwd)/tmp \
   -l outside-area ./all_polygons.ndgeojson
 
-  # tile-join -pk -f -o $OUTPUT_FILE ./all_points.mbtiles ./all_polygons.mbtiles
+# tile-join -pk -f -o $OUTPUT_FILE ./all_points.mbtiles ./xml_polygons.mbtiles ./all_polygons.mbtiles
+
+echo "end $(date +%s)"
---
+++ util/xml_polygon_generator.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+layer_name=$(basename "$1" .ndgeojson)
+ogr2ogr -f geojsonseq /vsistdout/ "$1" -sql "select st_convexhull(st_collect(geometry)) as geometry, \"市区町村コード\",
+\"市区町村名\", \"大字コード\", \"丁目コード\", \"小字コード\", \"大字名\", \"丁目名\", \"小字名\" from \"$layer_name\" GROUP BY '1'" -dialect sqlite
