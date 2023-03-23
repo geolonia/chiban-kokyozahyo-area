e3eb05d8a5b3de17366f38394d2fce345eb1afe8
naoki <n.globe.us@gmail.com>
2023-03-23 13:44:45 +0900

    筆の市区町村内外比率で判定する

--- kokyozahyo-outside-count.js
+++ kokyozahyo-outside-count.js
@@ -8,10 +8,11 @@ const outsideFiles = []
 const errorFiles = []
 
 const args = process.argv.slice(2)
-const prefCode = args[0] // 都道府県コードを第一引数で指定する
+// const prefCode = args[0] // 都道府県コードを第一引数で指定する
+const prefCode = "28" // 都道府県コードを第一引数で指定する
 
-const files = glob.sync(`../all_zips/${prefCode}*.ndgeojson`);
-// const files = glob.sync(`./test/${prefCode}*.ndgeojson`);
+// const files = glob.sync(`../all_zips/${prefCode}*.ndgeojson`);
+const files = glob.sync(`./test/${prefCode}*.ndgeojson`);
 
 // 地番住所の ndgeojson ファイルを読み込む
 for (const file of files) {
@@ -33,40 +34,46 @@ for (const file of files) {
   const city = JSON.parse(cityData)
   // 筆の ndgeojson ファイルを読み込む
   const raw = fs.readFileSync(file, "utf8");
-  const features = raw.split("\n")
 
+  const stringFeatures = raw.split("\n")
+  const 筆features = [];
   let is筆InsideCity;
 
-  for (const feature of features) {
-
-    // .split で最後の要素が空文字列になるので、それを除外する
-    if (!feature) {
+  for (const feature of stringFeatures) {
+    if (feature === "" ) {
       continue;
     }
 
     const 筆feature = JSON.parse(feature)
 
-    if (!筆feature.properties.地番.match(/^[0-9]/)) {
+    if (筆feature.properties.地番.match(/^地区外|別図/)) {
       continue;
     }
 
-    // 市区町村のポリゴンをループする
-    for (const cityFeature of city.features) {
+    筆features.push(筆feature)
+  }
 
-      const hullPolygon = turf.convex(筆feature)
-      is筆InsideCity = turf.booleanWithin(hullPolygon, cityFeature)
+  const hullPolygon = turf.convex(turf.featureCollection(筆features))
+  const hullPolygonArea = turf.area(hullPolygon)
 
-      if (is筆InsideCity) {
-        // 筆は市内にあるので、他の市区町村のポリゴンをチェックする必要はない
-        break;
-      }
-    }
+  // 市区町村のポリゴンをループする
+  for (const cityFeature of city.features) {
 
-    if (!is筆InsideCity) {
-      // "筆は市外にあるので、他のこのファイルの筆をチェックする必要はない"
-      outsideFiles.push([`${basename}.zip`])
+    const insideCityPolygon = turf.intersect(hullPolygon, cityFeature);
+    const insideCityPolygonArea = turf.area(insideCityPolygon)
+    const insideCityRatio = insideCityPolygonArea / hullPolygonArea
+
+    // 外に出ている面積が 5% 未満なら、市区町村の中に筆があると判断する
+    if (insideCityRatio > 0.95) {
+      is筆InsideCity = true;
       break;
+    } else {
+      is筆InsideCity = false;
     }
+  }
+
+  if (!is筆InsideCity) {
+    outsideFiles.push([`${basename}.zip`])
   }
 }
 
