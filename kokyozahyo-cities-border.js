a33e3bdff5bffd98b7dd3c6fff6fa1882a5a2ce4
naoki <n.globe.us@gmail.com>
2023-05-09 12:08:36 +0900

    add script for get cities border

--- kokyozahyo-cities-border.js
+++ kokyozahyo-cities-border.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
-// data/urls から z
+const xml2geojson = require("./util/xml2geojson")
+const fs = require("fs")
+
+
+const xml = fs.readFileSync("./all_zips/30422-1704-3.xml", "utf8")
+const geojson = xml2geojson(xml)
+console.log(JSON.stringify(geojson, null, 2))
---
+++ util/xml2geojson.js
@@ -0,0 +1,89 @@
+#!/usr/bin/env node
+const {proj4, JP_ZONE_TO_EPSG_MAP} = require("./proj")
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+const xml2geojson = (xmlString) => {
+
+  const geojson = {
+    "type": "FeatureCollection",
+    "features": []
+  }
+
+  const dom = new JSDOM(xmlString);
+  const parser = new dom.window.DOMParser();
+  const xmlDoc = parser.parseFromString(xmlString, "text/xml");
+
+  const projection = xmlDoc.getElementsByTagName('座標系')[0].textContent || ''
+  const name = xmlDoc.getElementsByTagName('地図名')[0].textContent || ''
+
+  const 筆界線s = xmlDoc.getElementsByTagName('筆界線')
+
+  if (! projection || '任意座標系' === projection) {
+    return
+  }
+  
+  for (let j = 0; j < 筆界線s.length; j++) {
+    const 筆界線 = 筆界線s[j]
+    const 線種別 = 筆界線.getElementsByTagName('線種別')[0]
+
+    if ('市区町村界線' !== 線種別.textContent) {
+      continue
+    }
+
+    const 形状 = 筆界線.getElementsByTagName('形状')[0]
+    const idref = 形状.attributes.getNamedItem('idref').value
+
+    if (idref) {
+      const line = xmlDoc.getElementById(idref)
+
+      if (line) {
+
+        const points = line.getElementsByTagName('zmn:GM_PointRef.point')
+        const coordinates = getCoordinates(xmlDoc, points)
+
+        const feature = {
+          type: "Feature",
+          geometry: {
+            type: "LineString",
+            coordinates: coordinates
+          },
+          properties: {
+            name: `${name} 市区町村界線`,
+            idref: idref
+          }
+        }
+
+        geojson.features.push(feature)
+      }
+    }
+  }
+
+  return geojson
+}
+
+const getCoordinates = (dom, nodes) => {
+  // @ts-ignore
+  const 座標系 = JP_ZONE_TO_EPSG_MAP[dom.getElementsByTagName('座標系')[0].textContent] || 'EPSG:4326'
+
+  if (nodes && 座標系) {
+    let coordinates = []
+
+    for (let j = 0; j < nodes.length; j++) {
+      // @ts-ignore
+      const node = dom.getElementById(nodes[j].attributes.getNamedItem('idref').value)
+      if (node) {
+        const x = node.getElementsByTagName('zmn:X')[0].textContent
+        const y = node.getElementsByTagName('zmn:Y')[0].textContent
+        if (座標系) {
+          const coordinate = proj4(座標系, 'EPSG:4326', [Number(y), Number(x)]);
+          coordinates.push(coordinate)
+        }
+      }
+    }
+
+    return coordinates
+  }
+}
+
+module.exports = xml2geojson
